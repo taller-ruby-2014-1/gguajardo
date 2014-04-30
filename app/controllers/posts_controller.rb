bd8d18913c951a009a7af4e81590fcb5a16b022c
gabriel guajardo <unhearted.6365@gmail.com>
2014-04-30 15:42:46 -0400

    Clase 5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,43 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		#render text: params[:post].inspect
-		@post = Post.new(post_params)
+		#@post = Post.new(post_params)
+		@post = Post.new(params[:post].permit(:title, :text))
 
-		@post.save
-		redirect_to @post
+		if 	@post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
 	end
 
 	private
